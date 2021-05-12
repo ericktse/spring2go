a6a85bbb423a1f824c645b3ce6d48d39e705f1bd
xiexiaobin <xiexiaobin@cedarhd.com>
2021-05-12 17:57:02 +0800

    feat:添加redis公共模块

--- spring2go-auth/src/main/java/com/spring2go/auth/controller/AuthorizeController.java
+++ spring2go-auth/src/main/java/com/spring2go/auth/controller/AuthorizeController.java
@@ -3,15 +3,19 @@ package com.spring2go.auth.controller;
 import com.spring2go.auth.domain.LoginModel;
 import com.spring2go.auth.service.InnerAuthorizeService;
 import com.spring2go.common.core.domain.R;
+import com.spring2go.common.redis.service.RedisService;
 import com.spring2go.upms.api.entity.SysUser;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description: 认证校验Controller
@@ -21,14 +25,20 @@ import javax.servlet.http.HttpServletRequest;
 @Api(tags = "认证校验")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class AuthorizeController {
 
     private final InnerAuthorizeService innerAuthorizeService;
+
+    @Autowired
+    private RedisService redisService;
 
     @PostMapping("login")
     public R<?> login(@RequestBody LoginModel model) {
         // 用户登录
         R result = innerAuthorizeService.login(model.getUsername(), model.getPassword());
+        redisService.setCacheObject("test", result, 60L, TimeUnit.SECONDS);
+        log.info("------------测试 Redis:"+redisService.getCacheObject("test"));
         return result;
     }
 
---
+++ spring2go-common/spring2go-common-redis/src/main/java/com/spring2go/common/redis/config/RedisAutoConfiguration.java
@@ -0,0 +1,48 @@
+package com.spring2go.common.redis.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring2go.common.redis.service.RedisService;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisServer;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @description: redis自动配置
+ * @author: xiaobin
+ * @date: 2021-05-12 16:34
+ */
+@Configuration
+public class RedisAutoConfiguration {
+
+    @Bean
+    public RedisService redisServer(RedisTemplate redisTemplate) {
+        return new RedisService(redisTemplate);
+    }
+
+    @Bean
+    @SuppressWarnings(value = {"unchecked", "rawtypes", "deprecation"})
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        FastJsonRedisSerializer serializer = new FastJsonRedisSerializer(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+
+        template.setValueSerializer(serializer);
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
